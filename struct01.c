1ef21a848f18861cde4c21e73a4ddb4dc662b266
qquack2 <alswo037@gmail.com>
2024-10-14 15:58:52 +0900

    7주차_1

--- struct01.c
+++ struct01.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
-#include <math.h>
+#include <time.h>
+#include <stdlib.h>
 
-struct point {
+#define SIZE 100
+
+
+typedef struct point {
 	int x;
 	int y;
-};
+} POINT;
 
-double dist(struct point p1, struct point p2) {
-	return	sqrt(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2));
-}
 int main()
 {
-	struct point p1 = { 10, 20 };
-	struct point p2;
-	double d;
+	POINT point[SIZE] = { 0 };
+	POINT temp = { 0,0 };
+	int least;
 
-	p1.x = 20;
-	p2.x = 30;
-	p2.y = 40;
-
-	printf("p1.x = %d, p1.y = %d\n", p1.x, p1.y);
-	printf("p2.x = %d, p2.y = %d\n", p2.x, p2.y);
-
-	printf("distance of p1, p2 = %lf", dist(p1, p2));
+	srand(time(NULL));
+	for (int i = 0; i < SIZE; i++) {
+		point[i].x = rand() % 101;
+		point[i].y = rand() % 101;
+	}
+	srand(time(NULL));
+	for (int i = 0; i < SIZE; i++) {
+		printf("point[%3d] : (%3d, %3d)\n", i, point[i].x, point[i].y);
+	}
+	for (int i = 0; i < SIZE - 1; i++) {
+		least = i;
+		for (int j = i + 1; j < SIZE; j++) {
+			if (point[least].x > point[j].x) least = j;
+			else if (point[least].x == point[j].x && point[least].y > point[j].y)
+				least = j;
+		}
+		temp = point[least];
+		point[least] = point[i];
+		point[i] = temp;
+	}
+	printf("after sorting>>>>>>>>>>>>>>\n");
+	for (int i = 0; i < SIZE; i++) {
+		printf("point[%3d] : (%3d, %3d)\n", i, point[i].x, point[i].y);
+	}
 
 	return 0;
 }
